82d07c299fdd45ebbe4d0fb9733f6386195c6cd7
GGICE <i@ice.gs>
2016-10-05 14:37:25 +0800

    eshint.

--- routes/add.js
+++ routes/add.js
@@ -1,4 +1,4 @@
-var mongoose = require("mongoose"),
+var mongoose = require('mongoose'),
   user = mongoose.model('user')
 
 module.exports = async function (ctx, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-var mongoose = require("mongoose")
+var mongoose = require('mongoose')
 
 module.exports = async function (ctx, next) {
     ctx.body = {
