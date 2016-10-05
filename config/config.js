42f8becd9f5f07c26dbaf9219bf44278e890a9d6
GGICE <i@ice.gs>
2016-10-05 14:24:08 +0800

    eshint.

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    'mongo' : {
-        'host' : '127.0.0.1',
-        'port' : '27017',
-        'database' : 'GSERVE'
-    },
-    'port' : '9001'
+  'mongo' : {
+    'host' : '127.0.0.1',
+    'port' : '27017',
+    'database' : 'userCenter'
+  },
+  'port' : '9901'
 }
--- models/user.js
+++ models/user.js
@@ -1,11 +1,11 @@
-var mongoose = require("mongoose")
+var mongoose = require('mongoose')
 var Schema = mongoose.Schema
 
 var userSchema = new Schema({
-    name : String,
-    password : String,
-    mail : String,
-    createTime : { type: Date, default: Date.now }
+  name : String,
+  password : String,
+  mail : String,
+  createTime : { type: Date, default: Date.now }
 })
 
-var user = mongoose.model('user', userSchema)
+mongoose.model('user', userSchema)
