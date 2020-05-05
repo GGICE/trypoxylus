1ded41226922bafec0bf8f6dce5ffb7f18f5d80f
GGICE <i@ice.gs>
2020-05-05 12:46:20 +0800

    feat: rm old code

--- config/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  'mongo' : {
-    'host' : '127.0.0.1',
-    'port' : '27017',
-    'database' : 'gserver'
-  },
-  'port' : '9901'
-}
--- controller/index.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require('mongoose')
-
-module.exports = async function (ctx, next) {
-  ctx.body = {
-    word: 'hello world!'
-  }
-}
