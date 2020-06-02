127785e8f1bcd5254a33084043c5a01477ab866d
GGICE <i@ice.gs>
2020-06-02 23:31:35 +0800

    feat: add types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import * as Koa from 'koa';
-import * as Router from 'koa-router';
+import Koa from 'koa';
+import Router from 'koa-router';
 import * as path from 'path';
 import { initRouters } from './router-manager';
 
--- src/router-manager/index.ts
+++ src/router-manager/index.ts
@@ -5,7 +5,7 @@ import * as Router from 'koa-router';
 import * as path from 'path';
 
 export function initRouters(app: Koa, router: Router, appPath: string) {
-  let routerPath = path.join(appPath, 'controller');
+  let routerPath = path.join(appPath, 'controllers');
 
   fs.readdirSync(routerPath).forEach((file) => {
     if (~file.indexOf('js') || ~file.indexOf('ts')) {
