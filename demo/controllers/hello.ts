2fb61f304a01104898c90b0f97b7fd6f962d8aa0
GGICE <i@ice.gs>
2020-06-09 01:05:59 +0800

    feat: days update

--- demo/controllers/hello.ts
+++ demo/controllers/hello.ts
@@ -1,8 +1,10 @@
-export default {
+import { CtrlModule } from 'trypoxylus';
+
+export const controller: CtrlModule = {
   router: '/hello',
   get: (ctx) => {
     ctx.body = {
       word: "hello world!",
     };
   }
-}
+};
--- demo/models/hello.d.ts
+++
@@ -1,4 +0,0 @@
-import * as Sequelize from 'sequelize';
-export declare const modelName = "user";
-export declare const attributes: Sequelize.ModelAttributes;
-export declare const modelOptions: Sequelize.ModelOptions;
