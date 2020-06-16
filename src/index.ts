afb13c0afd0600b8c97824fb0342d5c653e8f4a5
GGICE <i@ice.gs>
2020-06-17 00:41:51 +0800

    feat: use deno

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
-import Koa from 'koa';
-import Router from 'koa-router';
-import { initRouters } from './router-manager';
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+// import { initModel } from './model-manager';
+// import { initRouters } from './router-manager';
 
-const app = new Koa();
+const app = new Application();
 const router = new Router();
 const defualtPort = 3000;
 
-export function start(appPath: string, port?: number) {
+export function start(appPath?: string, port?: number) {
+  port = port || defualtPort;
+  // (global as any).APP_PATH  = appPath;
   app.use(router.routes())
     .use(router.allowedMethods());
 
-  initRouters(app, router, appPath);
-  app.listen(port || defualtPort);
+  // initRouters(app, router, appPath);
+  // initModel(app);
+  app.listen({ port });
+  console.log('Listening', port);
 }
