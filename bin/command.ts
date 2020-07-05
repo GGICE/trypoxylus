f0515e0c2d33229fda3e06fd46adfcd4590a5ed5
GGICE <i@ice.gs>
2020-07-06 00:49:19 +0800

    feat: rm cli bin env

--- bin/command.ts
+++ bin/command.ts
@@ -1,19 +1,19 @@
+// deno run --inspect-brk --allow-net --allow-read --allow-write --allow-plugin --unstable
+
 import Denomander from "https://deno.land/x/denomander/mod.ts";
 import { start } from "../src/mod.ts";
 
-export async function command () {
-  const program = new Denomander(
-    {
-      app_name: "Trypoxylus",
-      app_description: "Trypoxylus",
-      app_version: "0.0.1",
-    },
-  );
-  
-  program
-    .command("start [dir]", "start")
-    .option("-p, --port ", "Listen on port")
-    .parse(Deno.args);
-  
-  start(await Deno.realPath(program.dir), program.port);
-}
+const program = new Denomander(
+  {
+    app_name: "Trypoxylus",
+    app_description: "Trypoxylus",
+    app_version: "0.0.1",
+  },
+);
+
+program
+  .command("start [dir]", "start")
+  .option("-p, --port ", "Listen on port")
+  .parse(Deno.args);
+
+start(await Deno.realPath(program.dir), program.port);
--- bin/reload.ts
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env -S deno run --reload --allow-net --allow-read --allow-write --allow-plugin --unstable
-
-import { command } from "./command.ts";
-
-command();
